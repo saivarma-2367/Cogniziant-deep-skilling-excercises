6f85d08aefd22c80ab2046b5039afc1d8e97940a
saivarma-2367 <ssiravur@gitam.in>
2025-06-20 16:51:04 +0530

    first week excercises

---
+++ Week1/FactoryMethodPatternExample/Main.java
@@ -0,0 +1,23 @@
+package FactoryMethodPatternExample;
+
+import FactoryMethodPatternExample.DocumentFactory;
+import FactoryMethodPatternExample.WordDocumentFactory;
+import FactoryMethodPatternExample.PdfDocumentFactory;
+import FactoryMethodPatternExample.ExcelDocumentFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        DocumentFactory wordFactory = new WordDocumentFactory();
+        Document wordDoc = wordFactory.createDocument();
+        wordDoc.open();
+
+        DocumentFactory pdfFactory = new PdfDocumentFactory();
+        Document pdfDoc = pdfFactory.createDocument();
+        pdfDoc.open();
+
+        DocumentFactory excelFactory = new ExcelDocumentFactory();
+        Document excelDoc = excelFactory.createDocument();
+        excelDoc.open();
+    }
+}
+
